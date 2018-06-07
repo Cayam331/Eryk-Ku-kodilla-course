943efeb177569ac7fa7449a2a20db110fd94ffdd
cayam331 <eryk.ku@interia.pl>
2018-06-07 22:17:22 +0200

    Task 7.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,28 +1,34 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifer.PoemBeautifer;
-import com.kodilla.stream.beautifer.PoemDecorator;
-import com.kodilla.stream.iterate.NumbersGenerator;
-import com.kodilla.stream.lambda.ExecuteSaySomething;
-import com.kodilla.stream.lambda.SaySomething;
-import com.kodilla.stream.lambda.Processor;
-import com.kodilla.stream.lambda.Executor;
-import com.kodilla.stream.lambda.ExpressionExecutor;
-import com.kodilla.stream.reference.FunctionalCalculator;
+
+import com.kodilla.stream.book.BookDirectory;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 
 
 public class StreamMain {
     public static void main(String[] args) {
-        System.out.println("Welcome to module 7 - Stream");
 
-        PoemBeautifer poemBeautifer = new PoemBeautifer();
 
-        poemBeautifer.beautify("tekst", (text) -> text.toUpperCase());
-        poemBeautifer.beautify("TEKST", (text) -> text.toLowerCase());
-        poemBeautifer.beautify("tekst", (text) -> text + " 123");
-        poemBeautifer.beautify("tekst", (text) -> "ABC " + text);
+        Forum forum = new Forum();
+        Map<Integer, ForumUser> theResult = forum.getUserList().stream().filter(forumUser -> forumUser.getSex() == 'm').
+                filter(forumUser -> LocalDate.from(forumUser.getBirthDay()).until(LocalDate.now(), ChronoUnit.YEARS) > 20).
+                filter(forumUser -> forumUser.getNumberOfPosts() > 1).
+                collect(Collectors.toMap(ForumUser::getId, forumUser -> forumUser));
 
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
+        Iterator it = theResult.entrySet().iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+
+        }
     }
+
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/book/Book.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/book/Book.java
@@ -1,15 +1,22 @@
-package com.kodilla.stream.immutable;
+package com.kodilla.stream.book;
 
 public final class Book {
     private final String title;
     private final String author;
     private final int yearOfPublication;
+    private final String signature;
+
+    public String getSignature() {
+        return signature;
+    }
 
     public Book(final String title, final String author,
-                final int yearOfPublication) {
+                final int yearOfPublication, final String signature) {
         this.title = title;
         this.author = author;
         this.yearOfPublication = yearOfPublication;
+        this.signature = signature;
+
     }
 
     public String getTitle() {
@@ -28,8 +35,7 @@ public final class Book {
         return "Book{" +
                 "author='" + author + '\'' +
                 ", title='" + title + '\'' +
-                ", yearOfPublication=" + yearOfPublication +
-                '}';
+                ", yearOfPublication=" + yearOfPublication + ", signature='" + signature + '\'' + '}';
     }
 
 }
