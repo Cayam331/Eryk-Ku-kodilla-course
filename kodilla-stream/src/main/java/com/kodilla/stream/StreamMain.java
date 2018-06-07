a64ee7d1c55ce606bc797dd6e99109c289eed651
cayam331 <eryk.ku@interia.pl>
2018-06-07 18:38:33 +0200

    Task 7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,6 +2,7 @@ package com.kodilla.stream;
 
 import com.kodilla.stream.beautifer.PoemBeautifer;
 import com.kodilla.stream.beautifer.PoemDecorator;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExecuteSaySomething;
 import com.kodilla.stream.lambda.SaySomething;
 import com.kodilla.stream.lambda.Processor;
@@ -14,23 +15,14 @@ public class StreamMain {
     public static void main(String[] args) {
         System.out.println("Welcome to module 7 - Stream");
 
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
-
-        System.out.println("Calculating expressions with method references");
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
-
         PoemBeautifer poemBeautifer = new PoemBeautifer();
+
         poemBeautifer.beautify("tekst", (text) -> text.toUpperCase());
         poemBeautifer.beautify("TEKST", (text) -> text.toLowerCase());
         poemBeautifer.beautify("tekst", (text) -> text + " 123");
         poemBeautifer.beautify("tekst", (text) -> "ABC " + text);
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
