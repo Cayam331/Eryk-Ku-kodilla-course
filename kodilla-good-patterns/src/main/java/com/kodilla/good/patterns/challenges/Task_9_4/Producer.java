1c15f8469be71c75a4674d70dd460669b3413a19
cayam331 <eryk.ku@interia.pl>
2018-07-09 15:46:22 +0200

    Task 9.4

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Task_9_4/Producer.java
@@ -0,0 +1,7 @@
+package com.kodilla.good.patterns.challenges.Task_9_4;
+
+public interface Producer {
+    public String getName();
+    public Product getProduct();
+    public boolean process();
+}
