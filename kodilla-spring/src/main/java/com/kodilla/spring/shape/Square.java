9d7f58f4b6c966c26dd9fd95edd7635b51a68668
cayam331 <eryk.ku@interia.pl>
2018-07-13 20:05:04 +0200

    fix

--- kodilla-spring/src/main/java/com/kodilla/spring/shape/Square.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/shape/Square.java
@@ -1,8 +1,6 @@
 package com.kodilla.spring.shape;
 
-import org.springframework.stereotype.Component;
 
-@Component
 public class Square implements Shape{
     @Override
     public String getShapeName() {
